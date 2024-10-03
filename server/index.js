d137178eb30b27de73bdcc1b9903326aab9d8fae
octaviomcnaughton@gmail.com <octaviomcnaughton@gmail.com>
2024-10-02 17:19:06 -0700

    Added all current endpoints for each HTML and CSS file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // Import npm modules
 const express = require('express');
 const fs = require('fs').promises;
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const path = require('path');
 
 // Intialize Parameters and Settings
@@ -16,17 +16,17 @@ const dataPath = path.join(__dirname, 'data');
 const serverPath = path.join(__dirname);
 
 // Utility functions
-// function encrypt(phrase) {
-//     let encryptedPhrase = ""
-//     bcrypt.genSalt(saltRounds, (err, salt) => {
-//         console.log(`salt: ${salt}`);
-//         bcrypt.hash(phrase, salt, (err, hash) => {
-//             console.log(`hash: ${hash}`)
-//             encryptedPhrase = hash;
-//         });
-//     });
-//     return encryptedPhrase;
-// }
+function encrypt(phrase) {
+    let encryptedPhrase = ""
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        console.log(`salt: ${salt}`);
+        bcrypt.hash(phrase, salt, (err, hash) => {
+            console.log(`hash: ${hash}`)
+            encryptedPhrase = hash;
+        });
+    });
+    return encryptedPhrase;
+}
 
 // Intialize middleware
 app.use(express.static(clientPath));
@@ -36,10 +36,31 @@ app.use(express.json());
 // URI endpoints
 
 app.get('/', (req, res) => {
-    res.sendFile('src/landing-page.html', { root: clientPath });
+    res.status(200).sendFile('src/landing-page.html', { root: clientPath });
 });
 app.get('/landing-page.css', (req, res) => {
-    res.sendFile('css/landing-page.css', { root: clientPath });
+    res.status(200).sendFile('css/landing-page.css', { root: clientPath });
+});
+
+app.get('/login', (req, res) => {
+    res.status(200).sendFile('pages/login.html', { root: serverPath });
+});
+app.get('/login.css', (req, res) => {
+    res.status(200).sendFile('css/login.css', { root: serverPath });
+});
+
+app.get('/home', (req, res) => {
+    res.status(200).sendFile('pages/home.html', { root: serverPath });
+});
+app.get('/home.css', (req, res) => {
+    res.status(200).sendFile('css/home.css', { root: serverPath });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).sendFile('pages/about.html', { root: serverPath });
+});
+app.get('/about.css', (req, res) => {
+    res.status(200).sendFile('css/about.css', { root: serverPath });
 })
 
 
