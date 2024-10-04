af5ea113bb23f1f7b9f82bc4dd5a690d2fc4d310
octaviomcnaughton@gmail.com <octaviomcnaughton@gmail.com>
2024-10-04 16:14:58 -0700

    removed pauses in macro

--- update.bat
+++ update.bat
@@ -4,12 +4,9 @@ git checkout %branch%
 git pull origin main
 git add .
 git commit -m "%msg%"
-git status
-pause
 git push origin %branch%
 git checkout main
+git pull origin main
 git merge %branch%
 git push origin main
-git status
-pause
 git checkout %branch%
