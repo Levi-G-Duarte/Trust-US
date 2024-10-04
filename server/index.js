6073479dc7bc612ef44d01134050940ca75cc305
Nooneyouknowof <octaviomcnaughton@gmail.com>
2024-10-04 13:22:04 -0700

    form related endpoits and database management

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
 // Import npm modules
 const express = require('express');
 const fs = require('fs').promises;
-const bcrypt = require('bcrypt');
 const path = require('path');
-
-// Intialize Parameters and Settings
-const key = "$2b$10$0fiDpmLI1nW256wt1CpvZO";
-
+const { v4: uuidv4 } = require('uuid');
 
 // Intialize the server application
 const app = express();
@@ -14,8 +10,6 @@ const app = express();
 const clientPath = path.join(__dirname, '..', 'client');
 const dataPath = path.join(__dirname, 'data');
 const serverPath = path.join(__dirname);
-
-// Utility functions
 
 // Intialize middleware
 app.use(express.static(clientPath));
@@ -52,26 +46,34 @@ app.get('/about.css', (req, res) => {
     res.status(200).sendFile('css/about.css', { root: serverPath });
 });
 
-// Example Javascript for Endpoint:
+// -- Example Javascript for Endpoint -- 
+// let data = {email: "example@example.com", password: "admin"};
+// Object.keys(data).forEach(item => {data[item] = btoa(data[item])});
 // let req = await fetch("/login", {
 //     method: "POST",
 //     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ email: btoa("testuser"), password: btoa("1234") })
+//     body: JSON.stringify(data)
 // });
-// await req.text();
+// let result = await req.text();
+// sessionStorage.setItem('authCode', result);
 app.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
         let user = await fs.readFile(`${dataPath}/users.json`, 'utf-8');
         user = JSON.parse(user);
-        console.log(user, req.body);
-        const userIndex = user.findIndex(user => user.email === email && user.password === password);
+        const userIndex = user.findIndex(user => user.email === email);
         if (userIndex != -1) {
             let client = user[userIndex];
-            console.log(userIndex);
-            res.status(200).send(`Successful!`);
+            if (password === client.password) {
+                client.authCode = uuidv4();
+                client.expirarion = Math.floor(Date.now() / 1000)+3600;
+                fs.writeFile(`${dataPath}/users.json`, JSON.stringify(user, null, 2));
+                res.status(200).send(client.authCode);
+            } else {
+                res.status(409).send(`Incorrect password`);
+            }
         } else {
-            res.status(404).send('File not Found');
+            res.status(404).send('Account does not exist');
         }
     } catch (error) {
         console.error(error.message);
@@ -79,20 +81,64 @@ app.post('/login', async (req, res) => {
     }
 });
 
-
+//  -- Example Javascript for Endpoint --
+// let data = {firstname: "first", lastname: "last", email: "example@example.com", password: "admin"};
+// Object.keys(data).forEach(item => {data[item] = btoa(data[item])});
+// let req = await fetch("/signup", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(data)
+// });
+// await req.text();
 app.post('/signup', async (req, res) => {
     try {
-        const { firstname, lastname, email, password } = req.body;
-        console.log(req.body);
-        res.status(200).send(`Successful! ${JSON.stringify(req.body)}`);
+        let client = req.body;
+        const { email } = client;
+        let user = await fs.readFile(`${dataPath}/users.json`, 'utf-8') || JSON.stringify([]);
+        user = JSON.parse(user);
+        const userIndex = user.findIndex(user => user.email === email);
+        if (userIndex === -1) {
+            client.balance = 0;
+            client.FICO = 0;
+            
+            console.log(client);
+            user.push(client);
+            fs.writeFile(`${dataPath}/users.json`, JSON.stringify(user, null, 2));
+            res.status(200).send(`Successful!`);
+        } else {
+            res.status(401).send(`User already exists`);
+        }
     } catch (error) {
         console.error(error.message);
         res.status(500).send(error.message);
     }
 });
 
+app.get('/auth/:authCode', async (req, res) => {
+    try {
+        const {authCode} = req.params;
+        let user = await fs.readFile(`${dataPath}/users.json`, 'utf-8');
+        user = JSON.parse(user);
+        const userIndex = user.findIndex(user => user.authCode === authCode);
+        if (userIndex != -1) {
+            let client = user[userIndex];
+            if (client.expirarion > Math.floor(Date.now()/1000)) {
+                const {firstname, lastname, email} = client;
+                res.status(200).send(JSON.stringify({firstname, lastname, email}));
+            } else {
+                res.status(403).send("Forbidden, Authcode has expired");
+            }
+        } else {
+            res.status(400).send("Bad Request");
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(error.message);
+    }
+})
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
